4c827cbf5bd5012829fa5981e282ae9c1063f67c
Robert Sarno <robertsarno@roberts-mbp.lan>
2021-12-13 23:22:35 -0800

    added component fetures for homepage working on scss

---
+++ src/pages/homepage.component.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./homepage.styles.scss";
+import Directory from "../components/directory/directory.coponent";
+const HomePage = () => <Directory />;
+
+export default HomePage;
